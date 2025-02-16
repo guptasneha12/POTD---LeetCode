861418a94f88775ab3d1ddb94df4a4b2bc10ffe9
Sneha Gupta <guptasneha2003.sg@gmail.com>
2025-02-16 17:57:43 +0530

    Add initial C++ code

---
+++ Construct the Lexicographically Largest Valid Sequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+ 
+ bool solve(int i,int n,vector<int> &res,vector<bool> &used){
+    if(i>=res.size()) return true;
+
+    if(res[i]!=-1){
+       return solve(i+1,n,res,used);
+    }
+    
+    for(int num=n;num>=1;num--){
+        if(used[num]==true) continue;
+        //try
+        used[num]=true;
+        res[i]=num;
+        //explore
+        if(num==1){
+            if(solve(i+1,n,res,used)==true) return true;
+        }else{
+            int j=res[i]+i;
+            if(j<res.size() && res[j]==-1){
+                res[j]=num;
+                if(solve(i+1,n,res,used)==true) return true;
+                res[j]=-1;
+            }
+        }
+        // undo
+        used[num]=false;
+        res[i]=-1;
+    }
+    return false;
+
+ }
+
+
+    vector<int> constructDistancedSequence(int n) {
+      vector<int> res(2*n-1,-1);
+      vector<bool> used(n,false);
+      solve(0,n,res,used);
+      return res;  
+    }
+};
