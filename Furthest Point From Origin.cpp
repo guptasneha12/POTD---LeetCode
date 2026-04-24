3c28e0f4131623d511a8f238361d4f251f1eaacc
guptasneha12 <guptasneha2003.sg@gmail.com>
2026-04-24 23:26:56 +0530

    leetcode potd

---
+++ Furthest Point From Origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int n=moves.size();
+        int l=0,r=0,b=0;
+        for(int i=0;i<n;i++){
+            if(moves[i]=='L') l++;
+            else if(moves[i]=='R') r++;
+            else b++;
+        }
+        return abs(l-r)+b;
+    }
+};
